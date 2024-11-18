1650c16593019a813f11551ca283432df5889108
leevigong <dddddaeun88.88@gmail.com>
2024-11-19 01:21:14 +0900

    [#6] fix: cors 수정

--- app/config.py
+++ app/config.py
@@ -1,15 +1,16 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
-    "http://localhost",
-    "https://www.investment-up.shop"
+    "http://localhost",                 # 로컬 개발 환경
+    "http://localhost:3000",            # 프론트엔드의 특정 포트
+    "https://www.investment-up.shop"    # 배포된 프론트엔드
 ]
 
 def add_cors_middleware(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],       # origins 리스트에 있는 도메인만 허용
-        allow_credentials=True,      # 쿠키를 사용하는 경우 True로 설정
-        allow_methods=["*"],         # 모든 HTTP 메서드를 허용
-        allow_headers=["*"],         # 모든 HTTP 헤더 허용
+        allow_origins=origins,         # 정의된 origins 리스트를 사용
+        allow_credentials=True,        # 쿠키와 인증 정보 허용
+        allow_methods=["*"],           # 모든 HTTP 메서드를 허용
+        allow_headers=["*"],           # 모든 HTTP 헤더 허용
     )
